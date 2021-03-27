cc58889752c405907adbede176402821beb573a6
rbantug <ronbantug22@gmail.com>
2021-03-27 22:57:29 +0800

    Edits on Stripe Webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,11 +56,11 @@ exports.getCheckoutSession = catchAsyncErrors( async (req, res, next) => {
 }) */
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
-
-  await Booking.create({ tour, user, price });
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_details.email })).id;
+    const price = session.amount_total / 100;
+  
+    await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
